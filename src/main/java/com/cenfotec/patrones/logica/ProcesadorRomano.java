dd78de4afd4b9e568ee207ef33342184830b0f4c
Esteban Montes <estebanm_g@outlook.com>
2018-02-21 20:50:13 -0600

    Codigo final funcionando

--- src/main/java/com/cenfotec/patrones/logica/ProcesadorRomano.java
+++ src/main/java/com/cenfotec/patrones/logica/ProcesadorRomano.java
@@ -1,8 +1,6 @@
 package com.cenfotec.patrones.logica;
 
 public class ProcesadorRomano {
-	private int[] values = { 1000, 500, 100, 50, 40, 10, 9, 5, 4, 1 };
-	private String[] romans = { "M", "D", "C","L",  "XL",  "X",  "IX", "V",  "IV", "I" };
 	
 	public int convierte (String siglas) {
 		int value = evaluateString(siglas);
