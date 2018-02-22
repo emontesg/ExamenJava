a6ef5e6ef9f1d391a460964a51cbf1d04e74128c
Esteban Montes <estebanm_g@outlook.com>
2018-02-21 19:49:54 -0600

    test if string its empty

--- src/main/java/com/cenfotec/patrones/logica/ProcesadorRomano.java
+++ src/main/java/com/cenfotec/patrones/logica/ProcesadorRomano.java
@@ -5,8 +5,23 @@ public class ProcesadorRomano {
 	private String[] romans = { "M", "D", "C","L",  "XL",  "X",  "IX", "V",  "IV", "I" };
 	
 	public int convierte (String siglas) {
-		
+		if(siglas == "") {
+			return 0;
+		}
 		return 0;
 	}
+	
+	private int stringToNumber(String letter) {
+     switch (letter) {
+        case "I":  return 1;
+        case "V":  return 5;
+        case "X":  return 10;
+        case "L":  return 50;
+        case "C":  return 100;
+        case "D":  return 500;
+        case "M":  return 1000;
+        default:   return -1;
+     }
+  }
 
 }
