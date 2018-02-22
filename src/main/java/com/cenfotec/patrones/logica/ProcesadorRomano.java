b4016620d447b8d1ca8e89af802976240cdf646e
Esteban Montes <estebanm_g@outlook.com>
2018-02-21 19:42:21 -0600

    Romans and values in ProcesadorRomano

--- src/main/java/com/cenfotec/patrones/logica/ProcesadorRomano.java
+++ src/main/java/com/cenfotec/patrones/logica/ProcesadorRomano.java
@@ -1,6 +1,9 @@
 package com.cenfotec.patrones.logica;
 
 public class ProcesadorRomano {
+	private int[] values = { 1000, 500, 100, 50, 40, 10, 9, 5, 4, 1 };
+	private String[] romans = { "M", "D", "C","L",  "XL",  "X",  "IX", "V",  "IV", "I" };
+	
 	public int convierte (String siglas) {
 		
 		return 0;
